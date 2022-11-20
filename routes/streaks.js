c779c75c447f21e5b98480878751734db78a8f38
Adam EMMANUEL <adam.emmanuel.pro@gmail.com>
2022-11-20 13:53:10 +0100

    feat: add mongoose request to create streak

--- routes/streaks.js
+++ routes/streaks.js
@@ -1,11 +1,41 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const router = express.Router();
 
+const streakEventSchema = new mongoose.Schema({
+  date: Date,
+  createdAt: Date,
+  updatedAt: Date,
+});
+const streakSchema = new mongoose.Schema({
+  _id: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+    auto: true,
+  },
+  name: String,
+  startDate: Date,
+  endDate: Date,
+  isActive: Boolean,
+  events: [streakEventSchema],
+  createdAt: Date,
+  updatedAt: Date,
+});
+// machines: [mongoose.Types.ObjectId],
+
+const models = {
+  Streak: mongoose.model('Streak', streakSchema),
+};
+
 // GET
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
   const { ids = [] } = req.query;
   const streakIds = Array.isArray(ids) ? ids : JSON.parse(ids);
   console.log(ids, Array.isArray(streakIds));
+
+  const streaks = await models.Streak.find();
+
+  console.log({ streaks });
 
   res.json({
     status: 200,
@@ -22,8 +52,24 @@ router.get('/:id', function(req, res, next) {
 });
 
 // CREATE
-router.post('/', function(req, res, next) {
-  res.json(req.body);
+router.post('/', async function(req, res, next) {
+  const streakData = {
+    // name: String,
+    // startDate: Date,
+    // endDate: Date,
+    // isActive: Boolean,
+    // events: [streakEventSchema],
+    // createdAt: Date,
+    // updatedAt: Date,
+    ...req.body,
+    isActive: true,
+    events: [],
+    createdAt: (new Date()).toISOString(),
+    updatedAt: (new Date()).toISOString(),
+  };
+  await models.Streak.create(streakData);
+
+  res.json({ status: 200, data: streakData });
 });
 
 // UPDATE
